59be98c53c330d3cbb8c466da58a511d5cd9c0fa
Jan Berkel <jan.berkel@gmail.com>
2010-01-08 04:57:33 +0100

    simple command history

--- src/org/jruby/ruboto/IRB.java
+++ src/org/jruby/ruboto/IRB.java
@@ -3,14 +3,19 @@ package org.jruby.ruboto;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Selection;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.content.Context;
 import android.util.Log;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.List;
+import java.util.ArrayList;
 
 import org.jruby.Ruby;
 import org.jruby.RubyInstanceConfig;
@@ -25,7 +30,7 @@ public class IRB extends Activity
 {
     private Ruby ruby;
     private static final String TAG = "IRB";
-
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -57,33 +62,112 @@ public class IRB extends Activity
         
         ThreadContext context = ruby.getCurrentContext();
         DynamicScope currentScope = context.getCurrentScope();
-        DynamicScope newScope = new ManyVarsDynamicScope(new EvalStaticScope(currentScope.getStaticScope()), currentScope);
+        final DynamicScope scope = new ManyVarsDynamicScope(new EvalStaticScope(currentScope.getStaticScope()), currentScope);
+        final HistoryEditText et = (HistoryEditText)findViewById(R.id.edit);        
 
-        final DynamicScope scope = newScope;
-
-        final EditText et = (EditText)findViewById(R.id.edit);
-        et.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
-                if (arg1 == EditorInfo.IME_NULL) {
-                    Editable rubyCode = et.getText();
-                    if (rubyCode.length() == 0) return true;
-                    
-                    tv.append(rubyCode + "\n");
-                    try {
-                        String inspected = ruby.evalScriptlet(rubyCode.toString(), scope).inspect().asJavaString();
-                        tv.append("=> " + inspected + "\n");
-                    } catch (RaiseException re) {
-                        Log.w(TAG, "exception", re);
-                        
-                        re.printStackTrace(textViewStream);
-                    }
-                    tv.append(">> ");
-                    et.setText("");
-                    return true;
+        et.setLineListener(new HistoryEditText.LineListener() {                  
+            public void onNewLine(String rubyCode) {                                    
+                tv.append(rubyCode + "\n");
+                try {
+                    String inspected = ruby.evalScriptlet(rubyCode, scope).inspect().asJavaString();
+                    tv.append("=> " + inspected + "\n");
+                } catch (RaiseException re) {
+                    Log.w(TAG, "exception", re);                
+                    re.printStackTrace(textViewStream);
                 }
-                return false;
+                tv.append(">> ");
+                et.setText("");               
             }
-        });
+        });            
         tv.setText(">> ");
     }
+    
+
+    /** EditText with history (key down, key up) */
+    public static class HistoryEditText extends EditText implements
+      android.view.View.OnKeyListener,
+      TextView.OnEditorActionListener
+    { 
+      public interface LineListener {
+        void onNewLine(String s);
+      }     
+
+      private int cursor = -1;
+      private List<String> history = new ArrayList<String>();
+      private LineListener listener;
+      
+      public HistoryEditText(Context ctxt) {
+        super(ctxt);
+        initListeners();
+      }
+      
+      public HistoryEditText(Context ctxt,  android.util.AttributeSet attrs) {
+        super(ctxt, attrs);    
+        initListeners();
+      }
+      
+      public HistoryEditText(Context ctxt,  android.util.AttributeSet attrs, int defStyle) {
+        super(ctxt, attrs, defStyle);
+        initListeners();
+      }
+      
+      private void initListeners() {
+        setOnKeyListener(this);
+        setOnEditorActionListener(this);
+      }
+      
+      public boolean onEditorAction(TextView arg0, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_NULL) {
+          String line = getText().toString();
+          if (line.length() == 0) return true;           
+          history.add(line);        
+          cursor = history.size();
+                      
+          if (listener != null) {
+            listener.onNewLine(line);
+            return true;
+          }
+        }
+        return false;
+      }
+      
+      public void setLineListener(LineListener l) { this.listener = l; }
+      
+      public void setCursorPosition(int pos) {
+        Selection.setSelection(getText(), pos);
+      }
+      
+      public boolean onKey(View view, int keyCode, KeyEvent evt) {        
+        if (evt.getAction() == KeyEvent.ACTION_DOWN || evt.getAction() == KeyEvent.ACTION_MULTIPLE) {
+
+          if (cursor >= 0 && (keyCode == KeyEvent.KEYCODE_DPAD_UP || keyCode == KeyEvent.KEYCODE_DPAD_DOWN)) {                        
+            if (keyCode == KeyEvent.KEYCODE_DPAD_UP ) {
+              cursor -= 1;
+            } else {
+              cursor += 1;
+            }
+            
+            if (cursor < 0)
+              cursor = 0;
+            else if (cursor >= history.size()) {
+              cursor = history.size() - 1;
+            }
+            setText(history.get(cursor));
+            return true;
+          } 
+          /*
+          else if (evt.isAltPressed()) {            
+            if (keyCode == KeyEvent.KEYCODE_A) {
+                setCursorPosition(0);
+                return true;
+            } else if (keyCode == KeyEvent.KEYCODE_Z) {            
+                setCursorPosition(getText().length());
+                return true;
+            }         
+          }
+          */
+        }
+        return false;
+      }
+    }    
 }
