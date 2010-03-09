85a3d3ef89f1ace678868bc9961e6967178c429c
Scott Moyer <rscottmoyer@gmail.com>
2010-03-09 11:09:27 -0800

    Added: show using a ListView and launching a separate RubotoActivity.

--- assets/demo-scripts/demo-ruboto.rb
+++ assets/demo-scripts/demo-ruboto.rb
@@ -7,6 +7,7 @@ $activity.start_ruboto_activity "$ruboto_demo" do
       linear_layout(:orientation => :horizontal) do
         button :text => "Hello, World"
         button :text => "Hello, Ruboto"
+        button :text => "List"
       end
       @tv = text_view :text => "Click buttons or menu items:"
     end
@@ -19,7 +20,7 @@ $activity.start_ruboto_activity "$ruboto_demo" do
   end
 
   when_on_clicked do |view|
-    my_click view.getText
+    view.getText == "List" ? launch_list : my_click(view.getText)
   end
 
   def self.my_click(text)
@@ -27,5 +28,12 @@ $activity.start_ruboto_activity "$ruboto_demo" do
       @tv.setText "#{@tv.getText}\n#{text}"
       @et.setText text
   end
-end
 
+  def self.launch_list
+    self.start_ruboto_activity("$my_list") do
+      setTitle "Pick Something"
+      setup_content {list_view :list => ["Hello, World", "Hello, Ruboto"]}
+      when_item_clicked {|view, pos| toast(@list[pos]); finish}
+    end
+  end
+end
