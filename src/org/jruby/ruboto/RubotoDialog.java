a0cae97919be4f714af82776493517a61c829ec7
Scott Moyer <rscottmoyer@gmail.com>
2010-03-27 15:47:08 -0700

    New subclass of RubotoActivity to specify the Theme.Dialog style.

---
+++ src/org/jruby/ruboto/RubotoDialog.java
@@ -0,0 +1,11 @@
+package org.jruby.ruboto;
+
+/***********************************************************
+ * 
+ * Extends RubotoActivity to specify Theme.Dialog in
+ * the AndroidManifest.xml file.
+ *
+ */
+
+public class RubotoDialog extends RubotoActivity {
+}
