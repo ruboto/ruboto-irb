1ede938a7c10b6dfbb1cfce96ca9192d4d256f7d
rscottm <rscottmoyer@gmail.com>
2011-09-06 10:10:08 -0700

    separate IRB script code into subclass

---
+++ src/org/ruboto/irb/IRBScript.java
@@ -0,0 +1,114 @@
+package org.ruboto.irb;
+
+import java.io.BufferedWriter;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.util.Log;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import org.ruboto.Script;
+
+public class IRBScript extends Script {
+    public static final String UNTITLED_RB = "untitled.rb";
+    private static long scriptsDirModified = 0;
+
+    private String contents = null;
+
+    /*************************************************************************************************
+     *
+     * Static Methods: Scripts List
+     */
+
+    public static boolean scriptsDirChanged() {
+    	return scriptsDirModified != Script.getDirFile().lastModified();
+    }
+
+    public static List<String> list() throws SecurityException {
+        return IRBScript.list(new ArrayList<String>());
+    }
+
+    public static List<String> list(List<String> list) throws SecurityException {
+    	scriptsDirModified = Script.getDirFile().lastModified();
+        list.clear();
+        String[] tmpList = Script.getDirFile().list(RUBY_FILES);
+        Arrays.sort(tmpList, 0, tmpList.length, String.CASE_INSENSITIVE_ORDER);
+        list.addAll(Arrays.asList(tmpList));
+        return list;
+    }
+
+    /*************************************************************************************************
+    *
+    * Constructors
+    */
+
+    public IRBScript(String name) {
+        this(name, null);
+    }
+
+    public IRBScript(String name, String contents) {
+        super(name);
+        this.contents = contents;
+    }
+
+    /* Create a Script from a URL */
+    public static IRBScript fromURL(String url) {
+    	try {
+            String [] temp = url.split("/");
+        	DefaultHttpClient client = new DefaultHttpClient();
+        	HttpGet get = new HttpGet(url);
+        	BasicResponseHandler handler = new BasicResponseHandler();
+        	return new IRBScript(temp[temp.length -1], client.execute(get, handler));
+    	}
+    	catch (Throwable t) {
+    		return null;
+    	}
+    }
+
+    /*************************************************************************************************
+     *
+     * Attribute Access
+     */
+
+    public IRBScript setContents(String contents) {
+        if (contents == null || contents.equals("")) {
+            this.contents = "";
+        } else {
+            this.contents = contents;
+        }
+        return this;
+    }
+
+    public String getContents() throws IOException {
+        if (this.contents == null) {
+            this.contents = super.getContents();
+        }
+        return this.contents;
+    }
+
+    /*************************************************************************************************
+     *
+     * Script Actions
+     */
+
+    public void save() throws IOException {
+        BufferedWriter buffer = new BufferedWriter(new FileWriter(getFile()));
+        buffer.write(contents);
+        buffer.close();
+    }
+
+//    public String execute() throws IOException {
+//        return Script.execute(getContents());
+//    }
+
+    public boolean delete() {
+        return getFile().delete();
+    }
+}
