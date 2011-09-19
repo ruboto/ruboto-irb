c8ae539e478af009014ddd2030a1735f8043721b
rscottm <rscottmoyer@gmail.com>
2011-09-19 08:07:17 -0700

    Modify initialization

--- src/org/ruboto/RubotoBroadcastReceiver.java
+++ src/org/ruboto/RubotoBroadcastReceiver.java
@@ -1,54 +1,47 @@
 package org.ruboto;
 
-import java.io.IOException;
-
-public abstract class RubotoBroadcastReceiver extends android.content.BroadcastReceiver {
-    private String scriptName;
-    private String remoteVariable = "";
-
-  public static final int CB_RECEIVE = 0;
-
-    private Object[] callbackProcs = new Object[1];
+public class RubotoBroadcastReceiver extends android.content.BroadcastReceiver {
+    private String scriptName = null;
+    private boolean initialized = false;
 
     public void setCallbackProc(int id, Object obj) {
-        callbackProcs[id] = obj;
+        // Error: no callbacks
+        throw new RuntimeException("RubotoBroadcastReceiver does not accept callbacks");
     }
 	
-    public RubotoBroadcastReceiver setRemoteVariable(String var) {
-        remoteVariable = ((var == null) ? "" : (var + "."));
-        return this;
-    }
-
     public void setScriptName(String name){
         scriptName = name;
     }
 
-    public RubotoBroadcastReceiver(String scriptName) {
-        setScriptName(scriptName);
-        if (Script.isInitialized()) {
-            loadScript();
+    public RubotoBroadcastReceiver() {
+        this(null);
+    }
+
+    public RubotoBroadcastReceiver(String name) {
+        super();
+
+        if (name != null)
+            setScriptName(name);
+    }
+
+    public void onReceive(android.content.Context context, android.content.Intent intent) {
+        if (Script.setUpJRuby(context)) {
+            Script.defineGlobalVariable("$context", context);
+            Script.defineGlobalVariable("$broadcast_receiver", this);
+            Script.defineGlobalVariable("$intent", intent);
+
+            try {
+                if (scriptName != null && !initialized) {
+                    new Script(scriptName).execute();
+                    initialized = true;
+                } else {
+                    Script.execute("$broadcast_receiver.on_receive($context, $intent)");
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
     }
-
-    protected void loadScript() {
-        Script.put("$broadcast_receiver", this);
-        try {
-            new Script(scriptName).execute();
-        } catch(IOException e) {
-            throw new RuntimeException("IOException loading broadcast receiver script", e);
-        }
-    }
-
-    /****************************************************************************************
-     * 
-     *  Generated Methods
-     */
-
-  public void onReceive(android.content.Context context, android.content.Intent intent) {
-    if (callbackProcs[CB_RECEIVE] != null) {
-      Script.callMethod(callbackProcs[CB_RECEIVE], "call" , new Object[]{context, intent});
-    }
-  }
 
 }	
 
