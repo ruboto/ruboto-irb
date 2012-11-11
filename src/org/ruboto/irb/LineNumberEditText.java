84e2613860b0063c45c6e22df7349e7a53518388
rscottm <rscottmoyer@gmail.com>
2012-11-10 22:09:07 -0800

    More efficient drawing for line numbers

--- src/org/ruboto/irb/LineNumberEditText.java
+++ src/org/ruboto/irb/LineNumberEditText.java
@@ -3,8 +3,9 @@ package org.ruboto.irb;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.util.Log;
+import android.text.TextPaint;
+import android.text.DynamicLayout;
+import android.text.SpannableStringBuilder;
 import android.widget.EditText;
 
 /*********************************************************************************************
@@ -16,12 +17,18 @@ import android.widget.EditText;
  */
 
 public class LineNumberEditText extends EditText {
-    private int defaultLeftPadding = 0;
     private boolean showLineNumbers = true;
+    private int defaultLeftPadding = 0;
+    private int lineHeight = 0;
+	private int currentMagnitude = 0;
+	private int lineNumberWidth = 0;
+	private int lc = 1;
+	private int lineAscent;
     private Paint paint = new Paint();
-    private Rect lineRect = new Rect();
-    private int lineHeight;
-
+	private TextPaint textPaint = null;
+    private DynamicLayout layout = null;
+	private SpannableStringBuilder text = null;
+	
     /*********************************************************************************************
      *
      * Constructors
@@ -46,15 +53,16 @@ public class LineNumberEditText extends EditText {
     *
     * Init after inflation
     */
-   
+    
 	public void init() {
-    setHorizontallyScrolling(true);
+		setHorizontallyScrolling(true);
 	    paint.setColor(getTextColors().getDefaultColor() & 0xAAFFFFFF);
         paint.setTextSize(getTextSize() * 0.6f);
         defaultLeftPadding = getPaddingLeft();
-        lineHeight = getLineHeight();
+        lineHeight = getLineHeight();	
+		textPaint = new android.text.TextPaint(paint);
 	}
-	
+
     /*********************************************************************************************
     *
     * Show/hide line numbers
@@ -66,6 +74,7 @@ public class LineNumberEditText extends EditText {
 	
 	public void setShowLineNumbers(boolean trueOrFalse) {
 		showLineNumbers = trueOrFalse;
+		buildLineNumbers();
 		invalidate();
 	}
 	
@@ -75,10 +84,23 @@ public class LineNumberEditText extends EditText {
     */
    
 	@Override
-	public void onDraw(Canvas c) {
-    	setPadding(lineNumberPadding(), getPaddingTop(), getPaddingRight(), getPaddingBottom());
-		super.onDraw(c);
-		if (showLineNumbers) drawLineNumbers(c);
+	public void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+
+		if (getShowLineNumbers() && getLineCount() > 0) {
+			if (layout == null || lc != getLineCount()) {
+				buildLineNumbers();
+			}
+
+	        canvas.save();
+
+	        canvas.translate(getScrollX() - 200 + lineNumberWidth, getExtendedPaddingTop() - lineAscent); 
+        	canvas.clipRect(200 - lineNumberWidth, getScrollY() - getExtendedPaddingTop(), getPaddingLeft() + 200,
+							getBottom() - getTop() - getExtendedPaddingBottom() + getScrollY() - getExtendedPaddingTop() + lineAscent);
+        	layout.draw(canvas);
+
+			canvas.restore();
+		}
 	}
 	
     /*********************************************************************************************
@@ -86,34 +108,43 @@ public class LineNumberEditText extends EditText {
 	 * Everything needed to update the line numbers and positioning
 	 */
 	
-    private void drawLineNumbers(Canvas canvas) {
-        int scrollX = getScrollX();
-        int scrollY = getScrollY();
-        int topLineNumber = Math.max(1, ((scrollY - getExtendedPaddingTop()) / lineHeight));
-        int bottomLineNumber = Math.min(getLineCount(), topLineNumber + (int)(getHeight() / lineHeight));
-        
-    	canvas.save();
+	private void buildLineNumbers() {
+		if  (layout == null) {
+			text = new android.text.SpannableStringBuilder("1");
+			layout = new android.text.DynamicLayout(text, textPaint, 200, 
+							android.text.Layout.Alignment.ALIGN_OPPOSITE, 
+							0.0f, lineHeight, false);
+			lineAscent = layout.getLineAscent(0);
+		}
 
-        canvas.clipRect(0, 
-						getExtendedPaddingTop() + scrollY, 
-						getPaddingLeft() + scrollX,
-						getBottom() - getTop() - getExtendedPaddingBottom() + scrollY);
+		int newMagnitude = (!getShowLineNumbers() || getLineCount() == 0) ? 0 : (int)((Math.log10(getLineCount()) + 0.0001) + 1);
+		if (currentMagnitude != newMagnitude) {  
+        	lineNumberWidth = newMagnitude * (int)paint.getTextSize();          
+        	post(
+			  	new Runnable() {
+				  	public void run() {
+					  	setPadding(defaultLeftPadding + lineNumberWidth, getPaddingTop(), getPaddingRight(), getPaddingBottom());
+					}
+				});
+            currentMagnitude = newMagnitude;
+		}
 
-        for(int i = topLineNumber; i <= bottomLineNumber; i ++) {
-        	getLineBounds(i - 1, lineRect);
-            canvas.drawText(String.valueOf(i), defaultLeftPadding + scrollX, lineRect.bottom - 8, paint);
-        }
-
-        canvas.restore();
-    }
-    
-    private int lineNumberPadding() {
-    	int lineCount = getLineCount();
-    	
-    	if (!showLineNumbers || lineCount == 0) {
-    		return defaultLeftPadding;
-    	} else {
-        	return defaultLeftPadding + (int)((Math.floor(Math.log10(lineCount)) + 1) * paint.getTextSize());
-    	}
+		if (lc > getLineCount() && getLineCount() > 0) {
+	        text.replace(calculateLineNumberSize(getLineCount()), text.length(), "");
+		} else {
+	        for(int i = lc + 1; i <= getLineCount(); i++) {
+				text.append("\n" + i);
+			}
+		}
+		
+		lc = getLineCount() == 0 ? 1 :  getLineCount();
+	}
+	
+    private int calculateLineNumberSize(int i) {
+		int size = (i * 2) - 1;
+		for(int x = 1; x <= (int)(Math.log10(i) + 0.0001); x++) {
+			size += (i - (int)Math.pow(10, x) + 1);
+		}
+		return size;
     }
 }
