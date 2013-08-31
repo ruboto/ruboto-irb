2a8275c3aa07071fc4ab40fa407af5734ed30685
Scott Moyer <rscottmoyer@gmail.com>
2013-08-31 10:10:39 -0700

    Redo start up to match intent based Splash

--- src/org/ruboto/irb/IRBEntryPointActivity.java
+++ src/org/ruboto/irb/IRBEntryPointActivity.java
@@ -1,24 +1,16 @@
 package org.ruboto.irb;
 
-import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 
 import org.ruboto.JRubyAdapter;
 import org.ruboto.SplashActivity;
 
 public class IRBEntryPointActivity extends android.app.Activity {
-  public void onCreate(Bundle bundle) {
-    if (!JRubyAdapter.isInitialized()) {
-      showSplash();
-      finish();
-    }
-    super.onCreate(bundle);
-  }
-
   public void onResume() {
     if (JRubyAdapter.isInitialized()) {
       fireRubotoActivity();
+    } else {
+      showSplash();
     }
     super.onResume();
   }
@@ -27,7 +19,7 @@ public class IRBEntryPointActivity extends android.app.Activity {
   }
 
   private void showSplash() {
-    Intent splashIntent = new Intent(IRBEntryPointActivity.this, SplashActivity.class);
+    Intent splashIntent = new Intent(this, SplashActivity.class);
     startActivity(splashIntent);
   }
 }
