6898e3269cf7d8d5199058e504319fc41cbdd615
Scott Moyer <rscottmoyer@gmail.com>
2013-08-31 10:09:11 -0700

    Redo start up for shortcuts to scripts

--- src/org/ruboto/irb/ShortcutBuilder.java
+++ src/org/ruboto/irb/ShortcutBuilder.java
@@ -13,10 +13,22 @@ import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class ShortcutBuilder extends Activity implements OnItemClickListener {
+    public static final String SCRIPT_ACTION   = "org.ruboto.intent.action.LAUNCH_SCRIPT";
+    public static final String SCRIPT_CATEGORY = "android.intent.category.DEFAULT";
+    public static final String SCRIPT_NAME     = "org.ruboto.extra.SCRIPT_NAME";
+
     public static final String TAG = "Ruboto-IRB";
     
     private ArrayAdapter<String> adapter;
     private List<String> scripts;
+    
+    public static Intent intentForScript(String name) {
+      Intent sc_intent = new Intent();
+      sc_intent.setAction(SCRIPT_ACTION);
+      sc_intent.addCategory(SCRIPT_CATEGORY);
+      sc_intent.putExtra(SCRIPT_NAME, name);
+      return sc_intent;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,18 +51,14 @@ public class ShortcutBuilder extends Activity implements OnItemClickListener {
     }
 
     public void onItemClick(AdapterView<?> av, View v, int pos, long id) {
-		Intent sc_intent = new Intent();
-		sc_intent.setAction("org.ruboto.intent.action.LAUNCH_SCRIPT");
-		sc_intent.addCategory("android.intent.category.DEFAULT");
-		sc_intent.putExtra("org.ruboto.extra.SCRIPT_NAME", scripts.get(pos));
-
-		Intent intent = new Intent();
-		intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, sc_intent);
-		intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, scripts.get(pos));
-		intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, 
-				Intent.ShortcutIconResource.fromContext(this, R.drawable.icon));
-		
-		setResult((intent != null) ? RESULT_OK : RESULT_CANCELED, intent);
-		finish();
+	Intent intent = new Intent();
+	intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, 
+	                ShortcutBuilder.intentForScript(scripts.get(pos)));
+	intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, scripts.get(pos));
+	intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, 
+			Intent.ShortcutIconResource.fromContext(this, R.drawable.icon));
+	
+	setResult((intent != null) ? RESULT_OK : RESULT_CANCELED, intent);
+	finish();
     }
 }   
