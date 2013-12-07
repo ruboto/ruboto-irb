795004f209b44fa497698254c36dc6b4d89aa0fb
rscottm <rscottmoyer@gmail.com>
2013-12-07 09:39:07 -0800

    Update to 0.16.1.dev

--- src/org/ruboto/irb/IRBEntryPointActivity.java
+++ src/org/ruboto/irb/IRBEntryPointActivity.java
@@ -11,6 +11,7 @@ public class IRBEntryPointActivity extends android.app.Activity {
       fireRubotoActivity();
     } else {
       showSplash();
+      finish();
     }
     super.onResume();
   }
@@ -19,7 +20,16 @@ public class IRBEntryPointActivity extends android.app.Activity {
   }
 
   private void showSplash() {
-    Intent splashIntent = new Intent(this, SplashActivity.class);
-    startActivity(splashIntent);
+      Intent splashIntent = new Intent(this, SplashActivity.class);
+      splashIntent.putExtra(Intent.EXTRA_INTENT, futureIntent());
+      startActivity(splashIntent);
+  }
+
+  protected Intent futureIntent() {
+      if (!getIntent().getAction().equals(Intent.ACTION_VIEW)) {
+          return new Intent(getIntent()).setAction(Intent.ACTION_VIEW);
+      } else {
+          return getIntent();
+      }
   }
 }
--- src/org/ruboto/irb/ScriptLaunch.java
+++ src/org/ruboto/irb/ScriptLaunch.java
@@ -1,10 +1,4 @@
 package org.ruboto.irb;
 
-import android.content.Intent;
-
 public class ScriptLaunch extends org.ruboto.EntryPointActivity {
-  protected Intent futureIntent() {
-    return ShortcutBuilder.intentForScript(getIntent().getExtras().getString(ShortcutBuilder.SCRIPT_NAME));
-  }
 }
-
